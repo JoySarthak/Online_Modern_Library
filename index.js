9aa263d619e02da92527f196fff5284cb0593db6
JoySarthak <sarthakdutta2002@gmail.com>
2025-06-16 00:35:19 +0530

    removing bcrypt replacing with bcryptjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
 require('dotenv').config(); // Added JWT for session handling
